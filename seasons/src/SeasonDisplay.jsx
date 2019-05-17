d78719371ce3275b063793d3e021e724667b3ff0
AlonsoGD <dani.alonso.garcia@gmail.com>
2019-05-16 21:14:52 -0400

    190516

--- seasons/src/SeasonDisplay.jsx
+++ seasons/src/SeasonDisplay.jsx
@@ -1,3 +1,4 @@
+import "./SeasonsDisplay.css";
 import React from "react";
 
 const seasonConfig = {
@@ -24,10 +25,10 @@ const SeasonDisplay = (props) => {
   const { text, iconName } = seasonConfig[season]; // {text, iconName}
 
   return (
-    <div>
-      <i className={`${iconName} icon`} />
+    <div className={`season-display ${season}`}>
+      <i className={`${iconName} icon massive icon-left`} />
       <h1>{text}</h1>
-      <i className={`${iconName} icon`} />
+      <i className={`${iconName} icon massive icon-right`} />
     </div>
   );
 };
